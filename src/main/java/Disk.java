a5ae0466c131475557438ac42ddb1158e7d31db9
Madlenn <szymanek.majka@gmail.com>
2020-03-21 14:51:13 +0100

    amendments 2

--- src/main/java/Disk.java
+++ src/main/java/Disk.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import  java.util.concurrent.ThreadLocalRandom;
 import java.util.Scanner;
 
 public class Disk {
@@ -13,20 +14,21 @@ public class Disk {
     }
 
     public double areaMonteCarlo(int samplesCount) {
+
         double area, areaSquare, x, y;
         int k = 0;
+
         Random generator = new Random();
 
-        for (int i = 1; i <= samplesCount + 1; i++) {
-            x = generator.nextDouble() + generator.nextDouble();
-            y = generator.nextDouble() + generator.nextDouble();
+        for (int i = 0; i < samplesCount; i++) {
+            x = ThreadLocalRandom.current().nextInt(-(int)radius,(int)radius)+ThreadLocalRandom.current().nextDouble();
+            y = ThreadLocalRandom.current().nextInt(-(int)radius,(int)radius)+ThreadLocalRandom.current().nextDouble();
             if ((Math.pow(x, 2) + Math.pow(y, 2)) <= Math.pow(radius, 2)) {
                 k++;
             }
         }
-
-        areaSquare = 4 * radius * radius;
-        area = areaSquare * k / samplesCount;
+        areaSquare = 4* radius * radius;
+        area = areaSquare * ((double)k / (double)samplesCount);
         return area;
     }
 }
